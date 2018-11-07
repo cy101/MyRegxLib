525c02c94eb8a3edc9b31c0b97192901d16bec22
罗勇 <robortly@163.com>
2018-11-07 22:18:22 +0800

    初始化项目

---
+++ MyRegxLib/MyRegx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyRegxLib
+{
+    public class MyRegx
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello MyRegx:{DateTime.Now}");
+        }
+    }
+}
